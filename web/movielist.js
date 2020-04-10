8afa65a097d239bfa96377f9fdee5d2ba5548a7f
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-04-10 13:51:20 -0700

    Initial table population works

--- web/movielist.js
+++ web/movielist.js
@@ -3,10 +3,26 @@
 
 
 function handleMovielistResult(resultData) {
-    console.log("This is getting call, okay?");
+    console.log("This is getting called, yes?");
+
+    let movieTable = $("#movie-table");
+
+    for (let i = 0; i < resultData.length; i++) {
+        let rowHTML = "";
+        rowHTML += "<tr>";
+
+        rowHTML += "<td>" + resultData[i]["movie_title"] + " (" + resultData[i]["movie_year"] + ")" + "</td>";
+        rowHTML += "<td>" + resultData[i]["movie_director"] + "</td>";
+        rowHTML += "<td>" + "Don't care for now" + "</td>";
+        rowHTML += "<td>" + "Don't care for now" + "</td>";
+        rowHTML += "<td>" + resultData[i]["movie_rating"] + "</td>";
+
+        rowHTML += "</tr>";
+        movieTable.append(rowHTML);
+    }
 }
 
-jQuery.ajax({
+$.ajax({
    dataType: "json",
    method: "GET",
    url: "api/movielist",
