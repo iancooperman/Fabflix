4a6cad84328d8e2e2b8fd0e5cad149e84f4353ba
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-04-23 15:05:48 -0700

    Finished draft of LoginServlet

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import javax.xml.transform.Result;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -25,6 +26,7 @@ public class LoginServlet extends HttpServlet {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
+        JsonObject responseJsonObject = new JsonObject();
 
         try {
             Connection dbcon = dataSource.getConnection();
@@ -32,29 +34,41 @@ public class LoginServlet extends HttpServlet {
             String query = "SELECT password FROM customers WHERE email = '" + email + "';";
             ResultSet passwordRS = statement.executeQuery(query);
 
-            JsonObject responseJsonObject = new JsonObject();
-            if (email.equals("anteater") && password.equals("123456")) {
-                // Login success:
+            if (passwordRS.next()) {
+                String dbPassword = passwordRS.getString("password");
+                if (password == dbPassword) {
+                    // Login success:
 
-                // set this user into the session
-                request.getSession().setAttribute("user", new User(username));
+                    // set this user into the session
+                    request.getSession().setAttribute("user", new User(email));
 
-                responseJsonObject.addProperty("status", "success");
-                responseJsonObject.addProperty("message", "success");
+                    responseJsonObject.addProperty("status", "success");
+                    responseJsonObject.addProperty("message", "success");
 
-            } else {
+                } else {
+                    // Login fail
+                    responseJsonObject.addProperty("status", "fail");
+                    responseJsonObject.addProperty("message", "incorrect username or password");
+                }
+            }
+            else {
                 // Login fail
                 responseJsonObject.addProperty("status", "fail");
                 responseJsonObject.addProperty("message", "incorrect username or password");
             }
+
+            dbcon.close();
+            statement.close();
+            passwordRS.close();
         }
         catch (Exception e) {
-
+            responseJsonObject.addProperty("status", "fail");
+            responseJsonObject.addProperty("message", e.getMessage());
         }
 
-
-
-
-        response.getWriter().write(responseJsonObject.toString());
+        // Transmit data
+        PrintWriter out = response.getWriter();
+        out.write(responseJsonObject.toString());
+        out.close();
     }
 }
---
+++ src/main/java/User.java
@@ -0,0 +1,7 @@
+package main.java;
+
+public class User {
+    public User(String email) {
+
+    }
+}
