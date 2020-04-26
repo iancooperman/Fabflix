ec992af545fbbf92d54f08896386400593955b05
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-04-26 12:41:35 -0700

    Created BrowseByGenreServlet

---
+++ src/main/java/BrowseByGenreServlet.java
@@ -0,0 +1,9 @@
+package main.java;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "BrowseByGenreServlet", urlPatterns = "api/browseByGenre")
+public class BrowseByGenreServlet extends HttpServlet {
+
+}
