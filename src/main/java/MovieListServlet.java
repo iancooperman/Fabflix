63832c9d8637a46a041ecde2647a08fcc14dda2c
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-04-26 20:45:07 -0700

    Beginning almost complete rewrite of MovieListServlet

--- src/main/java/MovieListServlet.java
+++ src/main/java/MovieListServlet.java
@@ -29,123 +29,27 @@ public class MovieListServlet extends HttpServlet {
 
         try {
             Connection dbcon = dataSource.getConnection();
-
             Statement statement = dbcon.createStatement();
 
-            // Server-side input validation
-            String limitOption = request.getParameter("limit");
-            String limit;
-            switch (limitOption) {
-                // case "a" = 10 is redundant
-                case "b":
-                    limit = "25";
-                    break;
-                case "c":
-                    limit = "50";
-                    break;
-                case "d":
-                    limit = "100";
-                    break;
-                default:
-                    limit = "10";
-                    break;
-            }
+            // Retrieving parameters
+            String titleOption = request.getParameter("title"); // A LIKE pattern
+            String yearOption = request.getParameter("year"); // An integer > 0 but relatively close to 2020
+            String directorOption = request.getParameter("director"); // A LIKE pattern
+            String starOption = request.getParameter("star"); // A LIKE patten
+            String genreOption = request.getParameter("genre"); // An integer corresponding to the genreId
+            String limitOption = request.getParameter("limit"); // 10, 25, 50, or 100
+            String pageOption = request.getParameter("page"); // An integer > 0
+            String sortByOption = request.getParameter("sortBy"); // title_asc, title_desc, rating_asc, or rating_desc
 
-            String sortByOption = request.getParameter("sortBy");
-            String sortBy;
-            switch (sortByOption) {
-                // case "title" is redundant
-                case "rating":
-                    sortBy = "rating DESC, title ASC";
-                    break;
-                default:
-                    sortBy = "title ASC, rating DESC";
-                    break;
-            }
 
-            int page = Integer.parseInt(request.getParameter("page"));
-            if (page < 1) {
-                page = 1;
-            }
 
-            int offset = (page - 1) * Integer.parseInt(limit);
 
-            String query = String.format("SELECT id, title, year, director, rating " +
-                    "FROM movies, ratings " +
-                    "WHERE movies.id = ratings.movieId " +
-                    "ORDER BY %s " +
-                    "LIMIT %s OFFSET %d;", sortBy, limit, offset);
 
-            // perform the query
-            ResultSet rs = statement.executeQuery(query);
-
-            JsonArray jsonArray = new JsonArray();
-
-            while (rs.next()) {
-                String movieId = rs.getString("id");
-                String movieTitle = rs.getString("title");
-                String movieYear = rs.getString("year");
-                String movieDirector = rs.getString("director");
-                String movieRating = rs.getString("rating");
-
-                JsonObject jsonObject = new JsonObject();
-                JsonArray genreArray = new JsonArray();
-                JsonArray starArray = new JsonArray();
-
-                Statement genreStatement = dbcon.createStatement();
-
-                String genreQuery = "SELECT genres.name " +
-                                    "FROM genres, genres_in_movies " +
-                                    "WHERE genres.id = genres_in_movies.genreId " +
-                                        "AND genres_in_movies.movieId = '" + movieId + "'" +
-                                    "LIMIT 3";
-
-                ResultSet genreResultSet = genreStatement.executeQuery(genreQuery);
-
-                while (genreResultSet.next()) {
-                    String genreName = genreResultSet.getString("name");
-                    genreArray.add(genreName);
-                }
-
-                genreResultSet.close();
-
-                Statement starStatement = dbcon.createStatement();
-
-                String starQuery = "SELECT stars.id, stars.name " +
-                        "FROM stars, stars_in_movies " +
-                        "WHERE stars.id = stars_in_movies.starId " +
-                        "AND stars_in_movies.movieId = '" + movieId + "'" +
-                        "LIMIT 3";
-
-                ResultSet starResultSet = starStatement.executeQuery(starQuery);
-
-                while (starResultSet.next()) {
-                    JsonObject starInfo = new JsonObject();
-                    String starId = starResultSet.getString("id");
-                    String starName = starResultSet.getString("name");
-                    starInfo.addProperty("star_id", starId);
-                    starInfo.addProperty("star_name", starName);
-
-                    starArray.add(starInfo);
-                }
-
-                starResultSet.close();
-
-                jsonObject.addProperty("movie_id", movieId);
-                jsonObject.addProperty("movie_title", movieTitle);
-                jsonObject.addProperty("movie_year", movieYear);
-                jsonObject.addProperty("movie_director", movieDirector);
-                jsonObject.add("movie_genres", genreArray);
-                jsonObject.add("movie_stars", starArray);
-                jsonObject.addProperty("movie_rating", movieRating);
-
-                jsonArray.add(jsonObject);
-            }
-
+            // Bookkeeping things
             out.write(jsonArray.toString());
             response.setStatus(200);
 
-            rs.close();
+            resultSet.close();
             statement.close();
             dbcon.close();
         }
