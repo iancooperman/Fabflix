060203960e652436071177d5ad07bb2220ecb28e
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-05-02 10:58:50 -0700

    Added JS file to store movielist default values

---
+++ web/movielistDefaultValues.js
@@ -0,0 +1,10 @@
+let movielistDefaultValues = {
+    "title": "",
+    "year": 0,
+    "director": "",
+    "star": "",
+    "genre": 0,
+    "limit": 10,
+    "page": 1,
+    "sortBy": "rating_desc_title_asc"
+};
