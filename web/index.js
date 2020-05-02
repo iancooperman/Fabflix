e3a1085b624a1123bf05afb5d09a7774d4a3d4cf
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-05-02 16:47:38 -0700

    Extended movielist URLs on browse/search page

--- web/index.js
+++ web/index.js
@@ -5,7 +5,11 @@ function handleResult(resultData) {
         let genreId = resultData[i]["genreId"];
         let genreName = resultData[i]["genreName"];
 
-        let href = "movielist.html?genre=" + genreId;
+        let parameterObject = movielistDefaultValues;
+        parameterObject["genre"] = genreId;
+        let serializedParameters = $.param(parameterObject);
+
+        let href = "movielist.html?" + serializedParameters;
         let newHTML = "<li><a href='" + href + "'>" + genreName + "</a></li>";
         genreList.append(newHTML);
     }
@@ -44,13 +48,16 @@ function handleSubmission(eventObject) {
         year = "0";
     }
 
-
     let directorPattern = createTextPattern($("#director").val());
     let starPattern = createTextPattern($("#star").val());
 
-    let parameterObject = {"title": titlePattern, "year": year, "director": directorPattern, "star": starPattern};
-    let serializedParameters = $.param(parameterObject);
+    let parameterObject = movielistDefaultValues;
+    parameterObject["title"] = titlePattern;
+    parameterObject["year"] = year;
+    parameterObject["director"] = directorPattern;
+    parameterObject["star"] = starPattern;
 
+    let serializedParameters = $.param(parameterObject);
 
     let movielistURL = "movielist.html?" + serializedParameters;
     // let the redirection commence!
