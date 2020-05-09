3d1ec6f73dee384522779cfdf41161c10c8f4116
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-05-09 10:29:20 -0700

    Updated User.java to have an employee role

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -7,13 +7,15 @@ public class User {
     private final String ccId;
     private final String address;
     private final String email;
+    private final boolean isEmployee;
 
-    public User(String id, String firstName, String lastName, String ccId, String address, String email) {
+    public User(String id, String firstName, String lastName, String ccId, String address, String email, boolean isEmployee) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.ccId = ccId;
         this.address = address;
         this.email = email;
+        this.isEmployee = isEmployee;
     }
 }
