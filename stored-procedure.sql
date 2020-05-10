d2553fe2fc492ea0eb0d64756016644b8bea2e2a
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-05-10 11:02:05 -0700

    Additional work on add_movie

--- stored-procedure.sql
+++ stored-procedure.sql
@@ -1,9 +1,14 @@
 DELIMITER $$
 
-CREATE PROCEDURE add_movie (IN title VARCHAR(100), year INT(11), director VARCHAR(100), star VARCHAR(100), genre VARCHAR(32))
+CREATE PROCEDURE add_movie (IN title VARCHAR(100), IN year INT(11), IN director VARCHAR(100), IN star VARCHAR(100), IN genre VARCHAR(32))
 BEGIN
 
 
+    SET @movieInDB = (SELECT EXISTS( SELECT * FROM movies WHERE movies.title = @title AND movies.year = @year AND movies.director = @director) );
+
+    SET @maxMovieId = (SELECT max( id ) FROM movies);
+
+    SELECT CONCAT( "tt", LPAD( SUBSTR( @maxMovieId , 3 ) + 1, 7, 0 ) ) AS newMovieId;
 
 
 END
