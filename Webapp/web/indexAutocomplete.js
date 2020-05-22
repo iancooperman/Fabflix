46b91f839fbb81fff9c124d650ea3ef877cb720c
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-05-22 16:39:19 -0700

    Learned how to use suggestion object literals

--- Webapp/web/indexAutocomplete.js
+++ Webapp/web/indexAutocomplete.js
@@ -1,3 +1,6 @@
 $("#main_search_bar_input").autocomplete({
-    lookup: ["Toy Story", "Toy Story 2", "Toy Story 3"]
+    lookup: [{value:"Toy Story", data: "ts"}, {value:"Toy Story 2", data: "ts2"}, {value:"Toy Story 3", data: "ts3"}],
+    onSelect: function(suggestion) {
+        alert(suggestion.data);
+    }
 })
