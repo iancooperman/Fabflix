c21988837063362fe53615372e78d8c13347e831
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-05-22 20:21:18 -0700

    Finished draft of autocomplete servlet

--- Webapp/src/main/java/AutocompleteServlet.java
+++ Webapp/src/main/java/AutocompleteServlet.java
@@ -1,11 +1,15 @@
 package main.java;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -17,6 +21,8 @@ public class AutocompleteServlet extends HttpServlet {
     private Connection dbcon;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+
         try {
             dbcon = dataSource.getConnection();
 
@@ -38,15 +44,37 @@ public class AutocompleteServlet extends HttpServlet {
             statement.setString(1, againstParameters);
             ResultSet resultSet = statement.executeQuery();
 
+            JsonArray jsonArray = new JsonArray();
             while(resultSet.next()) {
-                
+                // retrieve results from resultSet
+                String movieId = resultSet.getString("id");
+                String movieTitle = resultSet.getString("title");
+                String movieYear = resultSet.getString("year");
+
+                // place results in a new JsonObject which will be put in the jsonArray
+                JsonObject movieObject = new JsonObject();
+                movieObject.addProperty("movie_id", movieId);
+                movieObject.addProperty("movie_title", movieTitle);
+                movieObject.addProperty("movie_year", movieYear);
+                jsonArray.add(movieObject);
             }
 
-
+            response.setStatus(200);
+            // close objects
+            statement.close();
+            resultSet.close();
         }
         catch (Exception e) {
             e.printStackTrace();
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("status", "fail");
+            jsonObject.addProperty("message", e.getMessage());
 
+            response.setStatus(500);
+            out.write(jsonObject.toString());
         }
+
+        // close the outgoing PrintWriter
+        out.close();
     }
 }
