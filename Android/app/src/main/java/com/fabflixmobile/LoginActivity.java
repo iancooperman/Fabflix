a9a7b4e31dbdb9376caa7fc19b92105296f9f1a9
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-05-24 15:43:19 -0700

    began linking login layout to login activity

--- Android/app/src/main/java/com/fabflixmobile/LoginActivity.java
+++ Android/app/src/main/java/com/fabflixmobile/LoginActivity.java
@@ -3,12 +3,34 @@ package com.fabflixmobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
+    private Button loginButton;
+    private TextView message;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        username = findViewById(R.id.editTextUsername);
+        password = findViewById(R.id.editTextPassword);
+        loginButton = findViewById(R.id.buttonLogin);
+        message = findViewById(R.id.textViewMessage);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString();
+                message.setText(user);
+            }
+        });
     }
 }
