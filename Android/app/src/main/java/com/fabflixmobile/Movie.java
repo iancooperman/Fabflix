302cba2af941d5c65dc57c04bfa60b0f17528078
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-05-25 13:00:19 -0700

    Created Movie class

---
+++ Android/app/src/main/java/com/fabflixmobile/Movie.java
@@ -0,0 +1,45 @@
+package com.fabflixmobile;
+
+import java.util.ArrayList;
+
+public class Movie {
+    private String title;
+    private String year;
+    private String director;
+    private ArrayList<String> actors;
+    private ArrayList<String> genres;
+
+    public Movie(String title, String year, String director) {
+        this.title = title;
+        this.year = year;
+        this.director = director;
+    }
+
+    public void addActor(String actor) {
+        actors.add(actor);
+    }
+
+    public void addGenre(String genre) {
+        genres.add(genre);
+    }
+
+    public String getTitle() {
+        return new String(title);
+    }
+
+    public String getYear() {
+        return new String(year);
+    }
+
+    public String getDirector() {
+        return new String(director);
+    }
+
+    public ArrayList<String> getActors() {
+        return new ArrayList<String>(actors);
+    }
+
+    public ArrayList<String> getGenres() {
+        return new ArrayList<String>(genres);
+    }
+}
