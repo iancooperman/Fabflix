30c660078bb31f1b6aad8a3ac1edbda3732971af
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-06-06 10:22:52 -0700

    Starting working on log_processing.py

---
+++ Webapp/log_processing.py
@@ -0,0 +1,20 @@
+import csv
+import sys
+
+
+
+
+def main():
+    log_file_names = sys.argv[1:]
+    for file_name in log_file_names:
+        print(file_name)
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
